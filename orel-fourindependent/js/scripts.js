b57dd2098cca5d24075bad1534d9247a2aaad88e
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-18 14:34:07 -0700

    interface logic complited

--- orel-fourindependent/js/scripts.js
+++ orel-fourindependent/js/scripts.js
@@ -4,3 +4,24 @@ let priceELement = document.querySelector("#price")
 
 form.addEventListener("submit", event => {
   event.preventDefault() // prevent refresh
+
+  // get the size of piza
+  let size = document.getElementById("size").value
+
+  // select all toppings
+  let toppingEls = document.querySelectorAll(".toppings")
+  let toppings = []
+
+  // loop through and get checked topping
+  for (let i = 0; i < toppingEls.length; i++) {
+    const topping = toppingEls[i];
+    if (topping.checked) {
+      toppings.push(topping.name)
+    }
+  }
+
+  let pizza = new Pizza(toppings, size)
+  console.log(pizza)
+  let price = pizza.calculateCost()
+  priceELement.innerHTML = "total price is: " + price;
+})
